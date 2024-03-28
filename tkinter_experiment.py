b09c8d073de1758fe27662fc65390a8f07258177
David López-Menchero González <162711589+Cheroman-PCs@users.noreply.github.com>
2024-03-28 02:48:24 +0100

    Update tkinter_experiment.py

--- tkinter_experiment.py
+++ tkinter_experiment.py
@@ -1,134 +1,197 @@
-from tkinter import Frame, BOTH, CENTER, END
-from ttkbootstrap import Window, Treeview, SUCCESS, HEADINGS
+from ttkbootstrap import Window, Frame, Label, Button, Treeview, Scrollbar, TOP, SUCCESS, HEADINGS, BOTH, CENTER, HORIZONTAL, VERTICAL, RIGHT, Y, BOTTOM, X, END, LEFT
+from tkinter.messagebox import askyesno, showwarning
 from cytoflow import Tube, ImportOp, ThresholdOp, DensityGateOp, FlowPeaksOp
 from numpy import argmax, sort
 from os import path
 from matplotlib.pyplot import subplots, close
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from csv import writer
 
 # Variables globales
 theme_name = "cyborg"
 application_title = "Tkinter experiment"
-f = r"C:\Users\david\Desktop\1.fcs"
+minimum_window_width = 800
+minimum_window_height = 600
+columns = ("file_name", "total_number_events", "number_cluster_events", "percentage_number_events_total", "mfi_cluster") # Definimos las columnas del treeview
+user_directory = path.expanduser("~") # Esto obtiene el directorio de usuario
+user_desktop_directory = path.join(user_directory, "Desktop") # Esto obtiene el directorio de escritorio de usuario
+fcs_file = r"C:\Users\david\Desktop\1.fcs"
 xchannel = "R1-A"
 ychannel = "B8-A"
 scale = "log"
 channel = "B4-A"
 cluster_name = "FlowPeaks"
 
-# Función principal que genera la interfaz gráfica del programa y carga todos los datos
+# Función principal del programa
 def main():
     '''
-    Función principal que genera la interfaz gráfica del programa y carga todos los datos
+    Función principal del programa
     '''
-    # Generamos la ventana con un tema específico y un título
-    root = Window(themename = theme_name)
-    root.title(application_title)
+    # Generamos la ventana del programa
+    window = generate_window()
     
-    # Posicionamos y dimensionamos la ventana en la pantalla
-    window_size_placement(root)
+    # Dimensionamos y posicionamos la ventana en la pantalla
+    window_size_placement(window)
 
     # El estado de la ventana será maximizado
-    # root.state("zoomed")
+    # window.state("zoomed")
 
     # Generamos la tabla de datos
-    generate_treeview(root)
+    treeview = generate_treeview(window)
+
+    # Generamos los botones
+    generate_buttons(window, treeview)
 
     # Generamos el hilo que genera la ventana del programa
-    root.mainloop()
+    window.mainloop()
 
-# Función que centra la ventana en la pantalla
-def window_size_placement(root, window_width = 800, window_height = 600):
+# Función que genera la ventana del programa
+def generate_window():
     '''
-    Función que centra la ventana en la pantalla
+    Función que genera la ventana del programa
+    '''
+    # Generamos la ventana con un tema específico, un título y unas dimensiones mínimas
+    window = Window(themename=theme_name)
+    window.title(application_title)
+    window.minsize(minimum_window_width, minimum_window_height)
+    
+    # Retornamos la ventana del programa
+    return window
+
+# Función que dimensiona y posiciona la ventana en la pantalla
+def window_size_placement(window):
+    '''
+    Función que dimensiona y posiciona la ventana en la pantalla
     '''
     # Obtiene las dimensiones de la pantalla
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
 
     # Calcula la posición del centro
-    position_top = int(screen_height / 2 - window_height / 2)
-    position_right = int(screen_width / 2 - window_width / 2)
+    position_top = int(screen_height / 2 - minimum_window_height / 2)
+    position_right = int(screen_width / 2 - minimum_window_width / 2)
 
     # Posiciona la ventana en el centro de la pantalla
-    root.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+    window.geometry(f"{minimum_window_width}x{minimum_window_height}+{position_right}+{position_top}")
 
 # Función que genera la tabla con los datos
-def generate_treeview(root):
+def generate_treeview(window):
     '''
     Función que genera la tabla con los datos
     '''
-    # Creamos un frame en que generaremos el treeview
-    frame = Frame(root)
-    frame.place(relx = 0, rely = 0, relwidth = 1, relheight = 0.5)
-
-    # Definimos las columnas
-    columns = ("nombre_fichero", "numero_eventos_total", "numero_eventos_cluster_interes", "porcentaje_representa_numero_eventos_cluster_interes_sobre_total", "imf_cluster_interes")
+    # Creamos un frame en el que generaremos el treeview
+    treeview_frame = Frame(window)
+    treeview_frame.place(relx=0.2, rely=0, relwidth=0.8, relheight=0.5)
 
     # Creamos el Treeview
-    treeview = Treeview(frame, bootstyle = SUCCESS, columns = columns, show = HEADINGS) # NOTA: Para mostrar la columna #0, poner el atributo show = TREEHEADINGS
-    treeview.pack(fill = BOTH, expand = True)
-    treeview.column("nombre_fichero", minwidth = 350, width = 350, anchor = CENTER)
-    treeview.column("numero_eventos_total", minwidth = 100, width = 100, anchor = CENTER)
-    treeview.column("numero_eventos_cluster_interes", minwidth = 125, width = 125, anchor = CENTER)
-    treeview.column("porcentaje_representa_numero_eventos_cluster_interes_sobre_total", minwidth = 150, width = 150, anchor = CENTER)
-    treeview.column("imf_cluster_interes", minwidth = 75, width = 75, anchor = CENTER)
+    treeview = Treeview(treeview_frame, bootstyle=SUCCESS, columns=columns, show=HEADINGS) # NOTA: Para mostrar la columna #0, poner el atributo show=TREEHEADINGS
+    treeview.pack(fill=BOTH, expand=True, padx=(0, 20))
+    treeview.column(columns[0], minwidth=162, width=162, anchor=CENTER)
+    treeview.column(columns[1], minwidth=100, width=100, anchor=CENTER)
+    treeview.column(columns[2], minwidth=125, width=125, anchor=CENTER)
+    treeview.column(columns[3], minwidth=150, width=150, anchor=CENTER)
+    treeview.column(columns[4], minwidth=75, width=75, anchor=CENTER)
 
     # Definimos las cabeceras
-    treeview.heading("nombre_fichero", text = "Nombre fichero")
-    treeview.heading("numero_eventos_total", text = "Nº eventos total")
-    treeview.heading("numero_eventos_cluster_interes", text = "Nº eventos cluster")
-    treeview.heading("porcentaje_representa_numero_eventos_cluster_interes_sobre_total", text = "% nº eventos sobre total")
-    treeview.heading("imf_cluster_interes", text = "IMF cluster")
+    treeview.heading(columns[0], text="File name")
+    treeview.heading(columns[1], text="Total no. of events")
+    treeview.heading(columns[2], text="No. cluster events")
+    treeview.heading(columns[3], text="% no. of events over total")
+    treeview.heading(columns[4], text="MFI cluster")
 
-    # Añadimos los datos al Treeview
-    add_data_treeview(root, treeview)
+    # Añadimos los scrollbars del treeview
+    add_treeview_scrollbars(treeview_frame, treeview)
 
-# Función que añade los datos a la tabla
-def add_data_treeview(root, treeview):
+    # Añadimos los datos al treeview
+    treeview = add_data_treeview(window, treeview)
+
+    return treeview
+
+# Función que añade los scrollbars del treeview
+def add_treeview_scrollbars(treeview_frame, treeview):
     '''
-    Función que añade los datos a la tabla
+    Función que añade los scrollbars del treeview
     '''
-    treeview.insert("", END, values = new_experiment(root, f))
+    vertical_scrollbar = Scrollbar(treeview_frame, orient=VERTICAL, command=treeview.yview)
+    horizontal_scrollbar = Scrollbar(treeview_frame, orient=HORIZONTAL, command=treeview.xview)
+    treeview.configure(yscrollcommand=vertical_scrollbar.set, xscrollcommand=horizontal_scrollbar.set)
+    vertical_scrollbar.pack(side=RIGHT, fill=Y, padx=5, pady=(0, 20))
+    horizontal_scrollbar.pack(side=BOTTOM, fill=X, pady=5)
+
+# Función que añade los datos al treeview
+def add_data_treeview(window, treeview):
+    '''
+    Función que añade los datos al treeview
+    '''
+    treeview.insert("", END, values=new_experiment(window, fcs_file))
+    treeview.insert("", END, values=("A"))
+    treeview.insert("", END, values=("B"))
+    treeview.insert("", END, values=("C"))
+    treeview.insert("", END, values=("D"))
+    treeview.insert("", END, values=("E"))
+    treeview.insert("", END, values=("F"))
+    treeview.insert("", END, values=("G"))
+    treeview.insert("", END, values=("H"))
+    treeview.insert("", END, values=("I"))
+    treeview.insert("", END, values=("J"))
+    treeview.insert("", END, values=("K"))
+    treeview.insert("", END, values=("L"))
+    treeview.insert("", END, values=("M"))
+    treeview.insert("", END, values=("N"))
+    treeview.insert("", END, values=("Ñ"))
+    treeview.insert("", END, values=("O"))
+    treeview.insert("", END, values=("P"))
+    treeview.insert("", END, values=("Q"))
+    treeview.insert("", END, values=("R"))
+    treeview.insert("", END, values=("S"))
+    treeview.insert("", END, values=("T"))
+    treeview.insert("", END, values=("U"))
+    treeview.insert("", END, values=("V"))
+    treeview.insert("", END, values=("W"))
+    treeview.insert("", END, values=("X"))
+    treeview.insert("", END, values=("Y"))
+    treeview.insert("", END, values=("Z"))
+
+    return treeview
 
 # Función en la que aplicamos operaciones sobre el experimento y retornamos: el nombre del fichero, el nº de eventos total, el nº de eventos del cluster de interés, el % que representa el nº de eventos del cluster de interés sobre el total y la IMF del cluster de interés
-def new_experiment(root, f):
+def new_experiment(window, fcs_file):
     '''
     Función en la que aplicamos operaciones sobre el experimento y retornamos: el nombre del fichero, el nº de eventos total, el nº de eventos del cluster de interés, el % que representa el nº de eventos del cluster de interés sobre el total y la IMF del cluster de interés
     '''
-    tube = Tube(file = f)
+    tube = Tube(file=fcs_file)
 
-    import_op = ImportOp(tubes = [tube], channels = {xchannel : xchannel, ychannel : ychannel, channel : channel})
+    import_op = ImportOp(tubes=[tube], channels={xchannel : xchannel, ychannel : ychannel, channel : channel})
     experiment = import_op.apply()
 
     # Realizamos la operación Threshold sobre el experimento
     operation_name = "Threshold"
-    threshold_op = ThresholdOp(name = operation_name, channel = xchannel, threshold = 2000)
+    threshold_op = ThresholdOp(name=operation_name, channel=xchannel, threshold=2000)
     experiment_threshold = threshold_op.apply(experiment)
     experiment_threshold = experiment_threshold.query(operation_name)
 
     # Realizamos la operación DensityGate sobre el experimento
     operation_name = "DensityGate"
-    density_gate_op = DensityGateOp(name = operation_name, xchannel = xchannel, xscale = scale, ychannel = ychannel, yscale = scale, keep = 0.5)
+    density_gate_op = DensityGateOp(name=operation_name, xchannel=xchannel, xscale=scale, ychannel=ychannel, yscale=scale, keep=0.5)
     density_gate_op.estimate(experiment_threshold)
     experiment_density_gate = density_gate_op.apply(experiment_threshold)
     experiment_density_gate = experiment_density_gate.query(operation_name)
 
     # Realizamos la operación de clustering FlowPeaks sobre el experimento
-    flow_peaks_op = FlowPeaksOp(name = cluster_name, channels = [xchannel, ychannel], scale = {xchannel : scale, ychannel : scale}, h0 = 3)
+    flow_peaks_op = FlowPeaksOp(name=cluster_name, channels=[xchannel, ychannel], scale={xchannel : scale, ychannel : scale}, h0=3)
     flow_peaks_op.estimate(experiment_density_gate)
     experiment_flow_peaks = flow_peaks_op.apply(experiment_density_gate)
-    argmax(experiment_flow_peaks[[cluster_name]].groupby(by = experiment_flow_peaks[cluster_name]).count())
+    argmax(experiment_flow_peaks[[cluster_name]].groupby(by=experiment_flow_peaks[cluster_name]).count())
 
     # Una vez realizadas las opereaciones, pintamos la gráfica de puntos en la ventana del programa
-    generate_canvas(root, experiment_flow_peaks)
+    generate_canvas(window, experiment_flow_peaks)
 
     # Asignamos a variables los datos que queremos retornar del experimento
-    file_name = path.basename(f)
+    file_name = path.basename(fcs_file)
     total_number_events = experiment.data.shape[0]
     number_events_cluster_interest = experiment_flow_peaks.data.shape[0]
-    percentage_represents_number_events_cluster_interest_total = '{:.2%}'.format(number_events_cluster_interest / total_number_events)
-    mfi_cluster_interest = '{:.2f}'.format(median_fluorescence_intensity_cluster_interest(experiment_flow_peaks))
+    percentage_represents_number_events_cluster_interest_total = "{:.2%}".format(number_events_cluster_interest / total_number_events)
+    mfi_cluster_interest = "{:.2f}".format(median_fluorescence_intensity_cluster_interest(experiment_flow_peaks))
     
     # Retornamos: el nombre del fichero, el nº de eventos total, el nº de eventos del cluster de interés, el % que representa el nº de eventos del cluster de interés sobre el total y la IMF del cluster de interés
     return (file_name, total_number_events, number_events_cluster_interest, percentage_represents_number_events_cluster_interest_total, mfi_cluster_interest)
@@ -152,13 +215,13 @@ def median_fluorescence_intensity_cluster_interest(experiment_flow_peaks):
         return sorted_data[total_number_data // 2]
 
 # Función que pinta los eventos del cluster en una gráfica en la ventana del programa
-def generate_canvas(root, experiment_flow_peaks):
+def generate_canvas(window, experiment_flow_peaks):
     '''
     Función que pinta los eventos del cluster en una gráfica en la ventana del programa
     '''
-    # Creamos un frame en que generaremos el canvas
-    frame = Frame(root)
-    frame.place(relx = 0, rely = 0.5, relwidth = 1, relheight = 0.5)
+    # Creamos un frame en el que generaremos el canvas
+    canvas_frame = Frame(window)
+    canvas_frame.place(relx=0.2, rely=0.5, relwidth=0.8, relheight=0.5)
 
     data_frame = experiment_flow_peaks.data
 
@@ -169,18 +232,83 @@ def generate_canvas(root, experiment_flow_peaks):
     clusters = data_frame[cluster_name].unique()
     for cluster in clusters:
         data_frame_cluster = data_frame[data_frame[cluster_name] == cluster]
-        axes.scatter(data_frame_cluster[xchannel], data_frame_cluster[ychannel], label = f"{cluster_name} {cluster}", s = 5, color = "green")
+        axes.scatter(data_frame_cluster[xchannel], data_frame_cluster[ychannel], label=f"{cluster_name} {cluster}", s=5, color="green")
 
     # Mostramos la leyenda
     axes.legend()
     
     # Crear el canvas de tkinter y añadir la figura de matplotlib
-    figure_canvas_tk_agg = FigureCanvasTkAgg(figure, master = frame)
+    figure_canvas_tk_agg = FigureCanvasTkAgg(figure, master=canvas_frame)
     figure_canvas_tk_agg.draw()
-    figure_canvas_tk_agg.get_tk_widget().pack(fill = BOTH, expand = True)
-    close(figure)
+    figure_canvas_tk_agg = figure_canvas_tk_agg.get_tk_widget()
+    figure_canvas_tk_agg.pack(fill=BOTH, expand=True, padx=100, pady=20)
+    close(fig=figure)
+
+    # Botón "View graph in window"
+    # view_graph_window_button = Button(canvas_frame, text="View graph in window")
+    # view_graph_window_button.pack(side=RIGHT, padx=10, pady=20)
+
+# Función que genera los botones
+def generate_buttons(window, treeview):
+    '''
+    Función que genera los botones
+    '''
+    # Creamos un frame en el que generaremos los botones
+    buttons_frame = Frame(window)
+    buttons_frame.place(relx=0, rely=0, relwidth=0.2, relheight=1)
+
+    # Ponemos un componente de relleno en la parte superior para poder centrar los botones verticalmente
+    label1 = Label(buttons_frame)
+    label1.pack(side=TOP, expand=True)
+    
+    # Creamos los botones
+    # Botón "Load files"
+    load_files_button = Button(buttons_frame, text="Load files")
+    load_files_button.pack(side=TOP, pady=50)
+
+    # Botón "Delete"
+    delete_button = Button(buttons_frame, text="Delete", command=lambda: delete_row(treeview))
+    delete_button.pack(side=TOP, pady=50)
+
+    # Botón "Export"
+    export_button = Button(buttons_frame, text="Export", command=lambda: export_to_csv(treeview, columns))
+    export_button.pack(side=TOP, pady=50)
+
+    # Ponemos un componente de relleno en la parte inferior para poder centrar los botones verticalmente
+    label2 = Label(buttons_frame)
+    label2.pack(side=TOP, expand=True)
+
+# Función que borra filas
+def delete_row(treeview):
+    '''
+    Función que borra filas
+    '''
+    selected_items = treeview.selection() # Esto devuelve una lista de los ID de las filas seleccionadas
+    if selected_items: # Si hay alguna fila seleccionada
+        confirm = askyesno(title="Confirmation", message=f"Are you sure you want to delete {len(selected_items)} row(s)?") # Mostramos un popup para confirmar o no que queremos borrar las filas
+        if confirm: # Si hemos confirmado
+            for selected_item in selected_items: # Borramos las filas seleccionadas
+                treeview.delete(selected_item)
+    else: # En el caso de no haber ninguna fila seleccionada
+        showwarning(title="Warning", message="No row selected") # Mostramos un cuadro de diálogo de advertencia
+
+# Función que exporta el treeview a un fichero .csv
+def export_to_csv(treeview, columns):
+    '''
+    Función que exporta el treeview a un fichero .csv
+    '''
+    confirm = askyesno(title="Confirmation", message=f"Do you want to export the treeview in a .csv file?") # Mostramos un popup para confirmar o no que queremos el treeview en un fichero .csv
+    if confirm: # Si hemos confirmado
+        file_name = "treeview.csv"
+        csv_file = path.join(user_desktop_directory, file_name) # Esto une el directorio de escritorio de usuario y "treeview.csv" para formar una ruta completa donde se encuentra el archivo .csv
+        with open(csv_file, "w", newline="") as file:
+            file_writer = writer(file)
+            file_writer.writerow(columns)  # Escribimos los nombres de las columnas
+            for row_id in treeview.get_children():
+                row = treeview.item(row_id)["values"]
+                file_writer.writerow(row)
 
 # Si el nombre del módulo es igual a __main__ se ejecutará el código (esto se hace por si queremos utilizar este código como un módulo, y no queremos que ejecute el código del main)
 if __name__ == "__main__":
-    # Ejecutamos la función principal para generar la ventana del programa
+    # Ejecutamos la función principal del programa
     main()
