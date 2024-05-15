7204b9c8482504690c3026221048c6f4291e69ba
David López-Menchero González <162711589+Cheroman-PCs@users.noreply.github.com>
2024-05-15 21:07:21 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,20 +1,22 @@
-import os
 import sys
-from ttkbootstrap import Window, Toplevel, Label
+import os
+from ttkbootstrap import Window, Toplevel, Label, TOP, Button, PhotoImage
 from typing import Union
-from PIL import Image, ImageTk
+from PIL import Image
+from tkinter import messagebox
+import time
+from threading import Thread
 
 # Constantes
 _THEME_NAME: str = "cyborg"
 _APPLICATION_TITLE: str = "Loader"
-_BASE_ROUTE: str = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
-_LOADER_IMAGE_NAME: str = "icons\\loader.gif"
-_LOADER_IMAGE_PATH: str = os.path.join(_BASE_ROUTE, _LOADER_IMAGE_NAME) # Esto obtiene la imagen de la pantalla de bienvenida
 _MINIMUM_WINDOW_WIDTH: int = 800
 _MINIMUM_WINDOW_HEIGHT: int = 600
+_BASE_ROUTE: str = getattr(sys, "_MEIPASS", os.path.abspath(os.path.dirname(__file__)))
+_LOADER_IMAGE_NAME: str = "icons\\loader.gif"
+_LOADER_IMAGE_PATH: str = os.path.join(_BASE_ROUTE, _LOADER_IMAGE_NAME) # Esto obtiene la imagen del loader
+_0ALPHA: str = "-alpha"
 _0DISABLED: str = "-disabled"
-_MINIMUM_LOADER_TOPLEVEL_WIDTH: int = 200
-_MINIMUM_LOADER_TOPLEVEL_HEIGHT: int = 200
 
 # Función que genera la ventana del programa
 def generate_window() -> Window:
@@ -23,9 +25,10 @@ def generate_window() -> Window:
     """
     # Generamos la ventana con un tema específico, un título y unas dimensiones mínimas
     window: Window = Window(themename=_THEME_NAME)
-    window_size_placement(window) # Dimensionamos y posicionamos la ventana en la pantalla
     window.title(_APPLICATION_TITLE)
     window.minsize(_MINIMUM_WINDOW_WIDTH, _MINIMUM_WINDOW_HEIGHT)
+    window.configure(bg="red")
+    window_size_placement(window) # Dimensionamos y posicionamos la ventana en la pantalla
 
     # Actualizamos las tareas pendientes de la ventana principal
     window.update_idletasks()
@@ -38,16 +41,11 @@ def window_size_placement(window: Window) -> None:
     """
     Función que dimensiona y posiciona la ventana en la pantalla
     """
-    # Obtiene las dimensiones de la pantalla
-    screen_width: int = window.winfo_screenwidth()
-    screen_height: int = window.winfo_screenheight()
-
-    # Calcula la posición del centro
-    position_top: int = int(screen_height / 2 - _MINIMUM_WINDOW_HEIGHT / 2)
-    position_right: int = int(screen_width / 2 - _MINIMUM_WINDOW_WIDTH / 2)
-
+    # Dimensiona la ventana
+    window.geometry(f"{_MINIMUM_WINDOW_WIDTH}x{_MINIMUM_WINDOW_HEIGHT}")
+    
     # Posiciona la ventana en el centro de la pantalla
-    window.geometry(f"{_MINIMUM_WINDOW_WIDTH}x{_MINIMUM_WINDOW_HEIGHT}+{position_right}+{position_top}")
+    window.place_window_center()
 
 # Función que genera el nivel superior
 def generate_loader_toplevel(parent: Union[Window, Toplevel]) -> Toplevel:
@@ -56,10 +54,12 @@ def generate_loader_toplevel(parent: Union[Window, Toplevel]) -> Toplevel:
     """
     # Generamos el nivel superior con un título y unas dimensiones mínimas
     loader_toplevel: Toplevel = Toplevel(parent)
-    loader_toplevel_size_placement(parent, loader_toplevel) # Dimensionamos y posicionamos el nivel superior en la pantalla
+    loader_toplevel.attributes(_0ALPHA, 0.75) # Ajusta la transparencia del toplevel a semi-transparente
     parent.attributes(_0DISABLED, True) # Deshabilitamos la ventana principal
     loader_toplevel.overrideredirect(True) # Eliminamos la barra de título
+    loader_toplevel.focus_force() # Forzamos el foco
     loader_toplevel.grab_set() # Hacemos que la ventana sea modal
+    loader_toplevel_size_placement(parent, loader_toplevel) # Dimensionamos y posicionamos el nivel superior en la pantalla
 
     # Retornamos el nivel superior
     return loader_toplevel
@@ -77,51 +77,74 @@ def loader_toplevel_size_placement(parent: Union[Window, Toplevel], loader_tople
     position_x: int = parent.winfo_x()
     position_y: int = parent.winfo_y()
 
-    # Establecemos las dimensiones del toplevel
-    loader_toplevel.geometry(f"{_MINIMUM_LOADER_TOPLEVEL_WIDTH}x{_MINIMUM_LOADER_TOPLEVEL_HEIGHT}")
-
-    # Calculamos la posición de la ventana secundaria
-    loader_toplevel.geometry("+%d+%d" % (position_x + (parent_width - _MINIMUM_LOADER_TOPLEVEL_WIDTH) / 2, position_y + (parent_height - _MINIMUM_LOADER_TOPLEVEL_HEIGHT) / 2))
+    # Establecemos las dimensiones del toplevel y la centramos
+    # loader_toplevel.geometry(f"{parent_width + 2}x{parent_height + 32}+{position_x + 7}+{position_y}") # TODA LA VENTANA
+    loader_toplevel.geometry(f"{parent_width}x{parent_height}+{position_x + 8}+{position_y + 31}") # TODA LA VENTANA SIN LA BARRA DE TÍTULO NI BORDES
 
 def show_loader(parent: Union[Window, Toplevel], loader_image_path: str) -> Toplevel:
+    # Crear un Toplevel
     loader_toplevel: Toplevel = generate_loader_toplevel(parent)
-
-    # Cargar el GIF
+    
+    # Cargar la imagen .gif
     image: Image.Image = Image.open(loader_image_path)
-    frames: list[ImageTk.PhotoImage] = [ImageTk.PhotoImage(file=loader_image_path, format="gif -index %i" %(i)) for i in range(image.n_frames)]
-
+    frames: list[PhotoImage] = [PhotoImage(file=loader_image_path, format=f"gif -index {i}") for i in range(image.n_frames)]
+    
     def update(ind):
-        if label.winfo_exists(): # Verifica si la etiqueta todavía existe
-            frame = frames[ind]
-            ind += 1
-            if ind == len(frames):
-                ind = 0
-            label.configure(image=frame)
-            parent.after(100, update, ind)
+        frame = frames[ind]
+        ind += 1
+        if ind == len(frames):
+            ind = 0
+        loader_image_label.configure(image=frame)
+        loader_toplevel.after(100, update, ind)
 
-    parent.after(0, update, 0)
+    # Crear el widget para mostrar la imagen .gif
+    loader_image_label: Label = Label(loader_toplevel, background="")
+    loader_image_label.pack(side=TOP, expand=True)
 
-    # Crea una etiqueta para mostrar el GIF
-    label: Label = Label(loader_toplevel)
-    label.pack()
+    loader_toplevel.after(0, update, 0)
 
     return loader_toplevel
 
 def hide_loader(parent: Union[Window, Toplevel], loader_toplevel: Toplevel) -> None:
     parent.attributes(_0DISABLED, False) # Habilitamos la ventana principal de nuevo
-    loader_toplevel.destroy()
-    parent.deiconify()
+    loader_toplevel.destroy() # Detener la animación y cerrar la ventana
 
 # Si el nombre del módulo es igual a __main__ se ejecutará el código (esto se hace por si queremos utilizar este código como un módulo, y no queremos que ejecute el código del main)
 if __name__ == "__main__":
-    # Generamos la ventana del programa
+    # Crear la ventana principal
     window: Window = generate_window()
-    
-    # Muestra el loader
-    loader_toplevel: Toplevel = show_loader(window, _LOADER_IMAGE_PATH)
 
-    # Programa la destrucción del toplevel después de 5 segundos (5000 milisegundos)
-    window.after(5000, hide_loader, window, loader_toplevel)
+    # Función que se ejecuta al pulsar el botón
+    def clic() -> None:
+        """
+        Función que se ejecuta al pulsar el botón
+        """
+        # Función que ejecuta el trabajo
+        def work() -> None:
+            """
+            Función que ejecuta el trabajo
+            """
+            print("sleep time start")        
+            for i in range(10):
+                print(i)
+                time.sleep(1)
+            print("sleep time stop")
+
+            # Ocultamos el loader
+            hide_loader(window, loader_toplevel)
+
+        # Mostramos el loader
+        loader_toplevel: Toplevel = show_loader(window, _LOADER_IMAGE_PATH)
+        
+        # Arrancamos la función de trabajo en otro hilo y controlamos la excepción que pueda arrojar el hilo
+        try:
+            thread: Thread = Thread(target=work)
+            thread.start()
+        except Exception as exception:
+            messagebox.showerror(title="Error", message=f"Thread execution error:\n{exception}")
+
+    button: Button = Button(window, text="Work", command=clic)
+    button.pack(side=TOP, expand=True)
 
     # Generamos el hilo que genera la ventana del programa
     window.mainloop()
